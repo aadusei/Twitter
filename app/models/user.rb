598fa5f3958a68804b404205c8b4d705a6e5d62f
aadusei <b.adusei@yahoo.com>
2015-06-21 03:42:17 -0500

    Added FriendsDataAgain

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,24 @@ class User < ActiveRecord::Base
          validates_presence_of   :username
          validates_uniqueness_of :username
 
-         def request_friendship(user_2)
-  	         self.friendships.create(friend: user_2)
+         has_many :friendships, dependent: :destroy
+         has_many :inverse_frienships, class_name: "Frienship", foreign_key: "friend_id", dependent: :destroy
+
+         def request_frienship(user_2)
+  	         self.frienships.create(friend: user_2)
          end
 
+         def pending_friend_request_from
+         	   self.inverse_frienships.where(state: "pending")
+         end	
+
+         def pending_friend_request_to
+             self.frienships.where(state: "pending")
+         end  
+
+         def active_friend
+         	   self.frienships.where(state: "active").map(&:friend) + self.inverse_frienships.where(state: "active").map(&:user)
+         end	
+
 end
+ 
